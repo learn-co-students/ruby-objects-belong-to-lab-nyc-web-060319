4589d9aea642e407a1ada4792d9e074a00fc7ca1
TimeSmash <TimeSmash@github.com>
2019-06-07 22:10:22 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+
+
+
+class Song
+    
+    attr_accessor :name, :title, :artist
+
+    # def initialize(name)
+    #     @name = name
+    # end
+
+    # def artist=(artist)
+    #     @artist = artist
+    # end
+
+end
