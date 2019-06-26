a48d299ca3e2ebbe936f8ef8b56e18de9a13106c
Ingrid04 <ingridfmelgar@gmail.com>
2019-06-26 16:04:43 -0400

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,8 @@
+# require_relative "./artist.rb"
+# require_relative "./song.rb"
+
+# require_relative "./author.rb"
+# require_relative "./post.rb"
+
+# artist1 = Artist.new("Beyonce", "crazy in love")
+# song1 = Song.new("crazy in love", "Beyonce")
